bdefea15b4ff942aa0d6a1ca4cf9d49dafc0e5af
Bernabe Suarez <bsuarez@productos-ai.com>
2024-10-26 01:30:29 -0300

    Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import VoiceToText from "./components/VoiceToText";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <main className=" base_app flex justify-center items-center p-8 w-screen h-screen">
       <VoiceToText />
+      <Analytics />
     </main>
   );
 }
